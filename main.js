61aa633006192333992b6e610478538f600a3c1c
Prashant Verma <Prashantverma0612@gmail.com>
2024-01-21 15:57:50 +0530

    completed

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var answers1 = ["A", "C", "B"];
-var answers2 = ["A", "A", "B"];
-var answers3 = ["B", "A", "D"];
+var answers1 = ["A", "C", "A", "B", "B"];
+var answers2 = ["A", "A", "B" , "D", "A"];
+var answers3 = ["B", "A", "D" ,"B", "C"];
     tot = answers1.length;
 function getCheckedValue(radioName) {
     var radios = document.getElementsByName(radioName);
@@ -29,9 +29,41 @@ function returnScore(number) {
     "Your score is " + value + "/" + tot;
    
     if (number == 1) {
-        var cell = window.getElementById("val1");
-       
-        cell.innerHTML = value;
+        sessionStorage.setItem('quiz1', value);
        
     }
+    if (number == 2) {
+        sessionStorage.setItem('quiz2', value);
+       
+    }
+    if (number == 3) {
+        sessionStorage.setItem('quiz3', value);
+       
+    }
 }
+
+var cell1 = document.getElementById("val1");
+var cell2 = document.getElementById("val2");
+var cell3 = document.getElementById("val3");
+
+if (sessionStorage.getItem("quiz1")) {
+        
+cell1.innerHTML = sessionStorage.getItem('quiz1');
+}else {
+    cell1.innerHTML = "not attempted";
+}
+
+if (sessionStorage.getItem("quiz2")) {
+        
+cell2.innerHTML = sessionStorage.getItem('quiz2');
+}else {
+    cell2.innerHTML = "not attempted";
+}
+
+if (sessionStorage.getItem("quiz3")) {
+        
+cell3.innerHTML = sessionStorage.getItem('quiz3');
+}else {
+    cell3.innerHTML = "not attempted";
+}
+
